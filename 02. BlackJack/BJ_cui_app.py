0b7aed42c6e8a247c8993a856e55040329a34653
nagisakuze <156045677+nagisakuze@users.noreply.github.com>
2024-02-21 02:31:20 +0900

    Update BJ_cui_app.py

--- 02. BlackJack/BJ_cui_app.py
+++ 02. BlackJack/BJ_cui_app.py
@@ -1,61 +1,137 @@
 import random
 
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(suit, value))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw_card(self):
+        return self.cards.pop()
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def calculate_score(self):
+        score = 0
+        num_aces = 0
+
+        for card in self.cards:
+            if card.value.isnumeric():
+                score += int(card.value)
+            elif card.value in ['J', 'Q', 'K']:
+                score += 10
+            elif card.value == 'A':
+                num_aces += 1
+                score += 11
+
+        while num_aces > 0 and score > 21:
+            score -= 10
+            num_aces -= 1
+
+        return score
+
 class Player:
-    def __init__(self, hands, name):
-        self.hands = hands
-        self.name = name
-        self.hand = ""
+    def __init__(self):
+        self.hand = Hand()
 
-    def random_select(self):
-        self.hand = random.choice(list(self.hands.keys()))
+    def show_hand(self):
+        for card in self.hand.cards:
+            print(f"{card.value} of {card.suit}")
 
-    def select(self):
+class Dealer(Player):
+    def show_hand(self):
+        print(f"First card: {self.hand.cards[0].value} of {self.hand.cards[0].suit}")
+
+class BlackjackGame:
+    def __init__(self):
+        self.deck = Deck()
+        self.player = Player()
+        self.dealer = Dealer()
+
+    def play(self):
+        print("Welcome to Blackjack!")
+
+        # 初期の2枚のカードを引く
+        for _ in range(2):
+            self.player.hand.add_card(self.deck.draw_card())
+            self.dealer.hand.add_card(self.deck.draw_card())
+
+        # プレイヤーとディーラーの手札を表示
+        print("Your hand:")
+        self.player.show_hand()
+        print("\nDealer's hand:")
+        self.dealer.show_hand()
+
+        # プレイヤーのターン
         while True:
-            print("**じゃんけん ⇒ ", end="")
-            choice = input()
-            if choice == "q":
-                print("**強制終了します")
-                exit()
-            elif choice in self.hands.keys():
-                self.hand = choice
+            choice = input("Do you want to 'hit' or 'stand'? ").lower()
+
+            if choice == 'hit':
+                self.player.hand.add_card(self.deck.draw_card())
+                print("\nYour hand:")
+                self.player.show_hand()
+                if self.player.hand.calculate_score() > 21:
+                    print("Bust! You went over 21. You lose.")
+                    return
+            elif choice == 'stand':
                 break
             else:
-                print("**想定外の手です。{}の中から入力してください。".format(list(self.hands.keys())))
+                print("Invalid input. Please enter 'hit' or 'stand'.")
 
-    def show(self):
-        print("**{}の手：{}".format(self.name, self.hands[self.hand]))
+        # ディーラーのターン
+        print("\nDealer's turn:")
+        while self.dealer.hand.calculate_score() < 17:
+            self.dealer.hand.add_card(self.deck.draw_card())
+            self.dealer.show_hand()
 
-    def win_lose_check(self, enemy_hand):
-        if (self.hand == "0" and enemy_hand == "2") or \
-           (self.hand == "2" and enemy_hand == "5") or \
-           (self.hand == "5" and enemy_hand == "0"):
-            return True
+        # 勝敗判定
+        player_score = self.player.hand.calculate_score()
+        dealer_score = self.dealer.hand.calculate_score()
+
+        print("\nYour final hand:")
+        self.player.show_hand()
+        print(f"Your final score: {player_score}")
+
+        print("\nDealer's final hand:")
+        self.dealer.show_hand()
+        print(f"Dealer's final score: {dealer_score}")
+
+        if dealer_score > 21 or (player_score <= 21 and player_score > dealer_score):
+            print("You win!")
+        elif player_score == dealer_score:
+            print("It's a draw.")
         else:
-            return False
+            print("You lose.")
 
-def main():
-    print("Game Start")
-    hands_dict = {
-        "0": "グー",
-        "2": "チョキ",
-        "5": "パー"
-    }
-    
-    player = Player(hands_dict, "Player")
-    opponent = Player(hands_dict, "Opponent")
-
-    while True:
-        player.select()
-        opponent.random_select()
-        
-        player.show()
-        opponent.show()
-        
-        if player.win_lose_check(opponent.hand):
-            print("**You Win!!!")
-            break
-        else:
-            print("**You Lose!!!")
+    def play_again(self):
+        choice = input("Do you want to play again? (yes/no): ").lower()
+        return choice == 'yes'
 
 if __name__ == "__main__":
-    main()
+    while True:
+        game = BlackjackGame()
+        game.play()
+
+        if not game.play_again():
+            print("Thanks for playing. Goodbye!")
+            break
